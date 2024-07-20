4b0366d5806dbaa76fc8bac9c3f7fdf13f4ec33b
arpan <arpan.rec@gmail.com>
2024-07-20 23:15:04 +0530

    ip

--- plugins/filter/get_pub.py
+++ plugins/filter/get_pub.py
@@ -31,20 +31,9 @@ def get_unencrypted_id_ecdsa(id_ecdsa, password):
     return rsa_root_ca_openssh_no_pass_key_bytes.decode("utf-8")
 
 
-def onlyoffice_default_json_builder(current_json, pg_cert, pg_key, pg_ca):
-    current_json["services"]["CoAuthoring"]["sql"]["pgPoolExtraOptions"]["ssl"] = {
-        "rejectUnauthorized": False,
-        "ca": pg_ca,
-        "cert": pg_cert + "\n" + pg_ca,
-        "key": pg_key,
-    }
-    return current_json
-
-
 class FilterModule(object):
     def filters(self):
         return {
             "get_encrypted_id_ecdsa_pub_key": get_encrypted_id_ecdsa_pub_key,
             "get_unencrypted_id_ecdsa": get_unencrypted_id_ecdsa,
-            "onlyoffice_default_json_builder": onlyoffice_default_json_builder,
         }
