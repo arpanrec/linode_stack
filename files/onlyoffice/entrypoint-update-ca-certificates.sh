17ca9b42bb63868afd4d58f70b12040da90fba15
arpan <arpan.rec@gmail.com>
2024-07-26 03:23:26 +0530

    ip

--- files/onlyoffice/entrypoint-update-ca-certificates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sudo update-ca-certificates
-JSON_BIN=/var/www/${COMPANY_NAME}/documentserver/npm/json
-${JSON_BIN} -f /etc/onlyoffice/documentserver/default.json -I \
-    -e 'this.services.CoAuthoring.requestDefaults.agentOptions={}'
-ca_content=$(awk 'NF {sub(/\r/, ""); printf "%s\\n",$0;}' /usr/local/share/ca-certificates/ownca.crt)
-${JSON_BIN} -f /etc/onlyoffice/documentserver/default.json -I \
-    -e 'this.services.CoAuthoring.requestDefaults.agentOptions.ca="'"${ca_content}"'"'
-/app/ds/run-document-server.sh
