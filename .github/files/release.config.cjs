633de921f9df068e4f4fc312261f9e144a68e489
arpan <arpan.rec@gmail.com>
2024-11-17 19:20:25 +0530

    fix: update GitHub workflows to include timeout settings and cache dependency paths

--- .github/files/release.config.cjs
+++ .github/files/release.config.cjs
@@ -47,5 +47,6 @@ module.exports = {
                 message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
             },
         ],
+        ['@semantic-release/github'],
     ],
 };
